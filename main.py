7e2957728b4061e12073c0fcd40ea86941cf08af
Quantum BioTech <143538410+QuantumBioTech@users.noreply.github.com>
2023-10-23 22:43:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import transformers
+import torch
+
+model = "tiiuae/falcon-7b-instruct"
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    torch_dtype=torch.bfloat16,
+    trust_remote_code=True,
+    device_map="auto",
+)
+sequences = pipeline(
+   "can you write an email that I have created a syllabus for HTML and css and to kindly check it and let me know if you have any queries.:",
+    max_length=200,
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
